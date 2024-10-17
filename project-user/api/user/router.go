c8214e12a003c86e94b2a3f1c3d17af54f05898a
zmk-wawa <80107411+zmk-wawa@users.noreply.github.com>
2024-10-17 22:43:02 +0800

    批量路由，分离了路由的接口与实现

--- project-user/api/user/router.go
+++ project-user/api/user/router.go
@@ -1,6 +1,17 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"test.com/project-user/router"
+)
+
+func init() {
+	// 将这里(user)要注册的路由批量注册进去
+	log.Println("init user router")
+	// 传入一个或多个实现了Router接口的类
+	router.Register(&RouterUser{})
+}
 
 // 实现Router接口中的方法
 
--- project-user/router/router.go
+++ project-user/router/router.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"test.com/project-user/api/user"
 )
 
 //  类似Service接口层
@@ -12,22 +11,34 @@ type Router interface {
 	Route(r *gin.Engine)
 }
 
-//注册接口的类，不同的路由请求都要调用其来注册
-type RegisterRouter struct {
-}
+////注册接口的类，不同的路由请求都要调用其来注册
+//type RegisterRouter struct {
+//}
+//
+//// 构造函数
+//func NewRegisterRouter() *RegisterRouter {
+//	return &RegisterRouter{}
+//}
+//
+//// 用调用接口方法
+//func (*RegisterRouter) Route(ro Router, r *gin.Engine) {
+//	ro.Route(r)
+//}
 
-// 构造函数
-func NewRegisterRouter() *RegisterRouter {
-	return &RegisterRouter{}
-}
-
-// 用调用接口方法
-func (*RegisterRouter) Route(ro Router, r *gin.Engine) {
-	ro.Route(r)
-}
+// 用来批量注册路由
+var routers []Router
 
 // 在此注册路由
 func InitRouter(r *gin.Engine) {
-	router := NewRegisterRouter()
-	router.Route(&user.RouterUser{}, r)
+	//router := NewRegisterRouter()
+	//router.Route(&user.RouterUser{}, r)
+	// 批量注册
+	for _, ro := range routers {
+		ro.Route(r)
+	}
+}
+
+// 上层调用，将传入的Routers加进routers，等待最后批量注册
+func Register(ro ...Router) {
+	routers = append(routers, ro...)
 }
